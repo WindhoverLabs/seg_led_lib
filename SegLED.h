6ab531f07ca50e9f6feec09efda7a02b0521e883
lgomez@windhoverlabs.com <lgomez@windhoverlabs.com>
2023-09-24 20:04:06 -0500

    -Integrate FastLED

--- SegLED.h
+++ SegLED.h
@@ -7,6 +7,21 @@
 #define SegLED_H
 
 #include <Arduino.h>
+#include <FastLED.h>
+
+// How many leds in your strip?
+#define NUM_LEDS 60
+
+// For led chips like WS2812, which have a data line, ground, and power, you just
+// need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
+// ground, and power), like the LPD8806 define both DATA_PIN and CLOCK_PIN
+// Clock pin only needed for SPI based chipsets when not using hardware SPI
+#define DATA_PIN 6
+#define CLOCK_PIN 13
+
+// // Define the array of leds
+
+
 
 /*!
  * @brief Main SegLED class
@@ -15,11 +30,13 @@ class SegLED {
 public:
   SegLED();
   void helloWorld();
-
+  void configureLED(); // Maybe call it "begin" to follow conventions
+  void testLED();
 
 private:
   int16_t ac1, ac2, ac3, b1, b2, mb, mc, md;
   uint16_t ac4, ac5, ac6;
+  CRGB leds[NUM_LEDS];
 };
 
 #endif //  SegLED_H
